8a15da2eeed6e5381dcde98666f58d61e26d01bb
apratap <abhishek.vit@gmail.com>
2013-11-26 17:17:51 -0800

    bug fixing for second release

--- geneExpression_heatMap.R
+++ geneExpression_heatMap.R
@@ -3,16 +3,28 @@ source("memoised_pheatmap.R")
 
 
 #gene expression heatmap logic
-get_geneExpression_heatMap <- function(m,annotation,...){
-  
-  #change the data type to integer and 
+get_geneExpression_heatMap <- function(m,annotation='none',
+                                       clustering_distance_rows = "correlation",
+                                       clustering_distance_cols = "correlation",
+                                       explicit_rownames = 'none', ...){
+  #change the data type to integer and #mainly a sanity check
   # NOT FOR NOW : transform the count to log 2 scale
-  m <- apply(m,2,function(x) as.numeric(x))
+  m_numeric <- apply(m,2,as.numeric)
+  m <- m_numeric
+  
   
   #removing those genes which dont vary much across the samples
   # so any gene with SD < .2 across the samples will be dropped 
   drop_genes <- which(apply(m,1,sd) < .2)
-  m <-  m[-drop_genes,]
+  #following step to remove the bug seen 
+  #when m <-  m[-drop_genes,] is done directly and length(drop_genes) = 0
+  if(length(drop_genes) != 0){
+    m <-  m[-drop_genes,]  #filtering a mat , IMP
+    #also remove the same from the explicit rownames as those genes are taken out in anycase
+    explicit_rownames <- explicit_rownames[-drop_genes] #filtering a vector no , needed
+  }
+  
+  
   
   #check if remaining genes can be clustered
   if(nrow(m) < 3){
@@ -22,17 +34,32 @@ get_geneExpression_heatMap <- function(m,annotation,...){
   }
   else{ #do the clustering and heatmap
     #scaling genes across experiments
-    mat.scaled <- t(scale(t(m)))  
-    memoised_pheatmap(mat.scaled,
-                      scale="none",
-                      annotation = annotation,
-                      clustering_distance_rows = "correlation",
-                      clustering_distance_cols = "correlation",
-                      clustering_method = "average",
-                      border_color = NA,
-                      drawRowD = FALSE
-    )
+    mat.scaled <- t(scale(t(m))) 
+    #mat.scaled <- m
+    if(annotation != 'none'){
+      memoised_pheatmap(mat.scaled,
+                        scale="none",
+                        annotation = annotation,
+                        clustering_distance_rows = clustering_distance_rows,
+                        clustering_distance_cols = clustering_distance_cols,
+                        clustering_method = "average",
+                        border_color = NA,
+                        drawRowD = FALSE,
+                        explicit_rownames = explicit_rownames
+      )
+    }
+    else{
+      #no annotation
+      memoised_pheatmap(mat.scaled,
+                        scale="none",
+                        clustering_distance_rows = clustering_distance_rows,
+                        clustering_distance_cols = clustering_distance_cols,
+                        clustering_method = "average",
+                        border_color = NA,
+                        drawRowD = FALSE,
+                        explicit_rownames = explicit_rownames
+      )
+    }
   }
-  
 }
 
